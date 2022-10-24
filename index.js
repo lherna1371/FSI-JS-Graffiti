3289780ac572567646e8e6630821b3a6138c78f9
lherna1371 <lherna1371@gmail.com>
2022-10-24 12:31:38 -0500

    added css and updated js

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
 
 // Select some elements...
+let header = document.querySelector('#page-header');
+console.log(header)
+
+header.children[0].textContent = 'Luis Hernandez';
+header.style.background = 'green';
+header.style.textAlign = 'left';
+
+
+const images = document.querySelectorAll('.dog-image');
+for (let i = 0; i < images.length; i++) {
+    const element = images[i];
+    console.log(images[i])
+    element.style.borderRadius = '25%';
+    element.style.border = '5px solid red';
+}   
